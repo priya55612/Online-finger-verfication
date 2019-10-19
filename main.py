81510963045de2a4ef0628975488f6596481e8a3
Priyanka Reballi <44258409+priya55612@users.noreply.github.com>
2019-10-19 12:17:06 +0530

    Adding code for ridge segmentation

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 import cv2
 import numpy as np
 import orientation_estimation as oe
-
+import normalization as segmentation
 w = 3
 
 def main():
-    input_im = cv2.cvtColor(cv2.imread('finger_print.jpg'), cv2.COLOR_RGB2GRAY)
+    input_im = cv2.cvtColor(cv2.imread('C:/Users/Priyanka/Desktop/DIP_project/Online-finger-verfication/fingerprint.jpg'), cv2.COLOR_RGB2GRAY)
     # Estimating Local orientation at each place
-    localOrientation = oe.getLocalGradient(input_im, w)
-    cv2.imshow("localOrientation", localOrientation)
+    # localOrientation = oe.getLocalGradient(input_im.copy(), w)
+    # cv2.imshow("localOrientation", localOrientation)
+    # cv2.waitKey(0)
+    segmentedImage, mask = segmentation.ridge_segementation(input_im, 16,0.2)
+    cv2.imshow("segmentedImage", segmentedImage)
+    cv2.imshow("inputImage", input_im)
+    # cv2.waitKey(0)
+    print(mask.shape)
+    cv2.imshow("mask", (mask.astype(int)*255.0))
     cv2.waitKey(0)
-    print(input_im.shape)
+    #print(input_im.shape)
 
 
 if __name__ == '__main__':
---
+++ normalization.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def mean(img):
+    mean = np.mean(img)
+    return mean
+
+def std(img):
+    std = np.std(img)
+    return std
+
+def normalize(img):
+    norm_img = (img-mean(img))/std(img)
+    return norm_img
+
+def ridge_segementation(image, window_size, threshold):
+    rows, cols = image.shape
+    image = normalize(image)
+
+    padded_rows = np.int(window_size * np.ceil((np.float(rows))/(np.float(window_size))))
+    padded_cols = np.int(window_size * np.ceil((np.float(cols)) / (np.float(window_size))))
+
+    padded_img = np.zeros((padded_rows, padded_cols))
+
+    std_img = np.zeros((padded_rows, padded_cols))
+
+    padded_img[0:rows][:, 0:cols] = image
+
+    for i in range(0, padded_rows, window_size):
+        for j in range(0, padded_cols, window_size):
+            window = padded_img[i:i+window_size][:,j:j+window_size]
+            std_img[i:i+window_size][:,j:j+window_size] = np.std(window)*np.ones(window.shape)
+
+    std_img = std_img[0:rows][:,0:cols]
+
+
+    mask_img = std_img > threshold
+
+    mean_val = np.mean(image[mask_img])
+    std_val = np.std(image[mask_img])
+
+    norm_image = (image - mean_val) / (std_val)
+
+    return (norm_image, mask_img)
+
+
+
+
+
+
