2ac548f899baaae65d301ded20838bf4dd28a7f1
Umesh Sharnagat <umeshsn@amazon.com>
2019-10-19 18:38:42 +0530

    Optimisations to ridge_orientation algorithm

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import ridge_orientation as rio
 w = 3
 
 def main():
-    input_im = cv2.cvtColor(cv2.imread('C:/Users/Priyanka/Desktop/DIP_project/Online-finger-verfication/fingerprint.jpg'), cv2.COLOR_RGB2GRAY)
+    input_im = cv2.cvtColor(cv2.imread('finger_print.jpg'), cv2.COLOR_RGB2GRAY)
     # Estimating Local orientation at each place
     # localOrientation = oe.getLocalGradient(input_im.copy(), w)
     # cv2.imshow("localOrientation", localOrientation)
@@ -16,13 +16,10 @@ def main():
     segmentedImage, mask = segmentation.ridge_segementation(input_im, 16,0.2)
     cv2.imshow("segmentedImage", segmentedImage)
     cv2.imshow("inputImage", input_im)
-    # cv2.waitKey(0)
-    print(mask.shape)
     cv2.imshow("mask", (mask.astype(int)*255.0))
     # cv2.waitKey(0)
 
-    ridge_orientation_img = rio.ridge_orientation(segmentedImage, 1, 7, None)
-    print(np.max(ridge_orientation_img))
+    ridge_orientation_img = rio.ridge_orientation(segmentedImage, 1.2, 7, 7)
     print(np.min(ridge_orientation_img))
     # cv2.imshow("ridge_orientation_img", ridge_orientation_img)
     cv2.waitKey(0)
--- ridge_orientation.py
+++ ridge_orientation.py
@@ -1,38 +1,33 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage
 from scipy import signal
 
-def ridge_orientation(img, grad_sigma, window_sigma, orient_smooth_sigma):
+def ridge_orientation(img, grad_sigma, window_size, orient_smooth_sigma):
+    # Get size of gaussian filter for blurring. Discrete gaussian is calculated by convolving gaussian function
+    # in a window equals to 6 times sigma. Value outside this window can be neglected as they are very small.
     size = np.fix(6*grad_sigma)
     if np.remainder(size,2) == 0:
         size = size+1
     gaussian = cv2.getGaussianKernel(np.int(size),grad_sigma)
+    print("gaussian",gaussian)
     F = gaussian*gaussian.T
     F_y, F_x = np.gradient(F)
-    cv2.imshow("cgrafient ", F_x)
-    cv2.imshow("cgrafient2 ", F_y)
-    G_x = signal.convolve2d(img, F_x, mode="same")
-    G_y = signal.convolve2d(img, F_y, mode="same")
+    s_x = signal.convolve2d(img, F_x, mode="same")
+    s_y = signal.convolve2d(img, F_y, mode="same")
 
-    G_xx = np.power(G_x,2)
-    G_yy = np.power(G_y, 2)
-    G_xy = G_x*G_y
+    s_xx = s_x**2
+    s_yy = s_y**2
+    s_xy = s_x*s_y
 
-    size = np.fix(6*window_sigma)
+    # calculate theta
+    # theta = 1/2 * np.arctan(scipy.convolve2d(2*s_xy, np.ones((3,3),dtype=int))/scipy.convolve2d(s_xx*s_yy, np.ones((3,3),dtype=int)))
 
-    gaussian = cv2.getGaussianKernel(np.int(size), window_sigma)
-    F = gaussian*gaussian.T
+    denominator = np.sqrt(np.power(s_xy,2)+np.power((s_xx-s_yy),2)) + np.finfo(float).eps
 
-    G_xx = ndimage.convolve(G_xx,F)
-    G_yy = ndimage.convolve(G_yy,F)
-    G_xy = 2*ndimage.convolve(G_xy,F)
-
-    denominator = np.sqrt(np.power(G_xy,2)+np.power((G_xx-G_yy),2)) + np.finfo(float).eps
-
-    sin_2_theta = G_xy/denominator
-    cos_2_theta = (G_xx-G_yy)/denominator
+    #Analytic solution of principal direction
+    sin_2_theta = s_xy/denominator
+    cos_2_theta = (s_xx-s_yy)/denominator
 
     if orient_smooth_sigma:
         size = np.fix(6*orient_smooth_sigma)
