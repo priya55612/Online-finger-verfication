9581722bbd635696f2858c2aadc5eae4949bc239
Priyanka Reballi <44258409+priya55612@users.noreply.github.com>
2019-10-31 21:27:41 +0530

    representing bifurcation and endings in image

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import image_enhancement
 import input_image
 import minutiae_util
+import numpy as np
 w = 3
 
 def main():
@@ -12,9 +13,26 @@ def main():
     enhanced_image = image_enhancement.img_enhancement(input_im)
     skeleton_image = image_enhancement.img_skeleton(enhanced_image)
 
+    cv2.imshow("enhanced_image", enhanced_image.astype("int")*255.0)
     cv2.imshow("enhanced skeleton image", skeleton_image.astype('int')*255.0)
 
     ridge_endings, ridge_bifurcations, ridge_2s = minutiae_util.find_minutiae(skeleton_image)
+
+    ending_img = np.zeros(input_im.shape)
+    # bifurcation_img = np.zeros(input_im.shape, dtype="uint8")
+    ending_img = cv2.cvtColor(np.zeros(input_im.shape, dtype="uint8"), cv2.COLOR_GRAY2RGB)
+    bifurcation_img = cv2.cvtColor(skeleton_image.astype('uint8')*255, cv2.COLOR_GRAY2RGB)
+    for i in range(len(ridge_bifurcations)):
+        x, y = ridge_bifurcations[i].x, ridge_bifurcations[i].y
+        bifurcation_img[x][y] = (0,255,0)
+    for i in range(len(ridge_endings)):
+        x, y = ridge_endings[i].x, ridge_endings[i].y
+        ending_img[x][y] = (0,0,255)
+    cv2.imshow("bifurcation pts", bifurcation_img)
+    cv2.imshow("ending pts", ending_img)
+
+
+
     print(ridge_endings)
     print(ridge_bifurcations)
     print(ridge_2s)
