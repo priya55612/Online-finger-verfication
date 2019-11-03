11c51204c50c7a75c649be2e9e6c3bd6017e4202
Umesh Sharnagat <umeshsn@amazon.com>
2019-11-03 10:35:31 +0530

    "remove SIFT"
    This reverts commit df4b3ea043c272955aa7acf17699ecb9e0d67313.

--- sift.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-def sift(img, ridge_endings, ridge_bifurcations, ridge_2s):
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(img,None)
-    img=cv2.drawKeypoints(img,kp,img)
-    cv2.imshow("sift ", img)
