109631c519195d531a9522fbb00aa4e636926419
Umesh Sharnagat <umeshsn@amazon.com>
2019-11-23 17:41:54 +0530

    Adding optimisation, documentation and Readme file

--- image_enhancement.py
+++ image_enhancement.py
@@ -7,20 +7,19 @@ import numpy as np
 import cv2
 
 
-def img_enhancement(img):
-    clahe = cv2.createCLAHE(clipLimit=2.0,
-                            tileGridSize=(8, 8))  # contrast enhancement using adaptive histogram equalization
-    img = clahe.apply(img)
-    # cv2.imshow("clahe img", img)
-
+# Algorithm reference paper
+# '''Fingerprint Image Enhancement: Algorithm and Performance Evaluation'''
+# https://pdfs.semanticscholar.org/bd6d/e6c7fba04a67d30a4bd1261665e6f4745ea8.pdf
+def enhance_image(img):
     window_size = 16
     threshold = 0.1
+    # Gnerate fingerpringt mask from given image
     norm_img, mask = ridge_segementation(img, window_size, threshold)
-    # cv2.imshow("normal image", norm_img)
+    cv2.imshow("normal image", norm_img)
 
     grad_sigma = 1
-    window_size = 7
     orient_smooth_sigma = 7
+    # Find orientation of each pixel by using gradient of its surrounding
     oriented_img = ridge_orientation(img, grad_sigma, orient_smooth_sigma)
     # cv2.imshow("orientation image", oriented_img)
 
@@ -41,25 +40,28 @@ def img_enhancement(img):
     return new_img < -3
 
 
-def img_skeleton(img):
-    # Remove breaks in ridges by applying closing operation. This is a novelty of we can achieve this
+def skeletonize(img):
+    # Remove breaks in ridges by applying closing operation.
     closed_image = morphology.closing(img, np.ones((3, 3)))
     cv2.imshow("closed Image", closed_image.astype('int') * 255.0)
 
-    skeleton_image = morphology.skeletonize(img)
+    skeleton_image = morphology.skeletonize(closed_image)
     return skeleton_image
 
 
+# This function checks if a window has no foreground pixels on 3 or more boundaries. If so, mark the complete window background.
+# This removes any secluded point in image.
+# Repeat this with multiple windows sizes.
 def remove_noise(img):
-    input_img = np.array(img.astype('int')*1.0)
-    output_img = np.array(img.astype('int')*1.0)
+    input_img = np.array(img.astype('int') * 1.0)
+    output_img = np.array(img.astype('int') * 1.0)
 
     W, H = input_img.shape[:2]
     filtersizes = [4, 6, 8, 12, 20]
-    filtersizes = [4, 20]
+    # filtersizes = [4, 20]
     for filtersize in filtersizes:
-        for i in range(W - filtersize):
-            for j in range(H - filtersize):
+        for i in range(0, W - filtersize, filtersize):
+            for j in range(0, H - filtersize, filtersize):
                 filter0 = output_img[i:i + filtersize, j:j + filtersize]
 
                 flag = 0
@@ -78,13 +80,12 @@ def remove_noise(img):
 
 
 def perform_enhancement_and_morphology(img):
-    img = img_enhancement(img)
-    cv2.imshow("enhanced image", img.astype('int') * 255.0)
+    enhanced_img = enhance_image(img)
+    cv2.imshow("enhanced image", enhanced_img.astype('int') * 255.0)
     # Thinning
-    skeleton = img_skeleton(img)
-    cv2.imshow("skeleton image output", skeleton.astype('int') * 255.0)
-
-    skeleton = np.array(skeleton, dtype=np.uint8)
-    skeleton = remove_noise(skeleton)
-    cv2.imshow("skeleton after removing noise", skeleton.astype('int') * 255.0)
-    return skeleton
+    skeleton_img = skeletonize(enhanced_img)
+    cv2.imshow("skeleton image output", skeleton_img.astype('int') * 255.0)
+    # Remove the breaks and secluded elements in fingerprint.
+    skeleton_image_wo_noise = remove_noise(skeleton_img)
+    cv2.imshow("skeleton after removing noise", skeleton_image_wo_noise.astype('int') * 255.0)
+    return skeleton_image_wo_noise
