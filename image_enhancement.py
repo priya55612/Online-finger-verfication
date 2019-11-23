d0fb07f85d89163c2d8f1c0805089ed6bd203b41
Umesh Sharnagat <umeshsn@amazon.com>
2019-11-23 15:27:21 +0530

    rearranging code and adding documentation

--- image_enhancement.py
+++ image_enhancement.py
@@ -8,6 +8,11 @@ import cv2
 
 
 def img_enhancement(img):
+    clahe = cv2.createCLAHE(clipLimit=2.0,
+                            tileGridSize=(8, 8))  # contrast enhancement using adaptive histogram equalization
+    img = clahe.apply(img)
+    # cv2.imshow("clahe img", img)
+
     window_size = 16
     threshold = 0.1
     norm_img, mask = ridge_segementation(img, window_size, threshold)
@@ -23,10 +28,11 @@ def img_enhancement(img):
     window_size = 5
     min_wave_length = 5
     max_wave_length = 15
-    freq, mean_freq = ridge_frequency(norm_img, mask, oriented_img, block_size, window_size, min_wave_length, max_wave_length)
+    freq, mean_freq = ridge_frequency(norm_img, mask, oriented_img, block_size, window_size, min_wave_length,
+                                      max_wave_length)
     # cv2.imshow("freauency image", freq)
 
-    freq = mean_freq*mask
+    freq = mean_freq * mask
     k_x = 0.65
     k_y = 0.65
     new_img = ridge_filter(norm_img, oriented_img, freq, k_x, k_y)
@@ -34,12 +40,12 @@ def img_enhancement(img):
 
     return new_img < -3
 
-def img_skeleton(img):
-    # TODO: Remove breaks in ridges by applying closing operation. This is a novelty of we can achieve this
-    # closed_image = morphology.closing(enhanced_image, np.ones((3,3)))
-    # cv2.imshow("closed Image", closed_image.astype('int')*255.0)
 
-    # cv2.imshow("enanced image", enhanced_image.astype('int')*255.0)
+def img_skeleton(img):
+    # Remove breaks in ridges by applying closing operation. This is a novelty of we can achieve this
+    closed_image = morphology.closing(img, np.ones((3, 3)))
+    cv2.imshow("closed Image", closed_image.astype('int') * 255.0)
+
     skeleton_image = morphology.skeletonize(img)
     return skeleton_image
 
@@ -48,23 +54,37 @@ def remove_noise(img):
     input_img = np.array(img.astype('int')*1.0)
     output_img = np.array(img.astype('int')*1.0)
 
-    W,H = input_img.shape[:2]
-    filtersizes = [4,20]
+    W, H = input_img.shape[:2]
+    filtersizes = [4, 6, 8, 12, 20]
+    filtersizes = [4, 20]
     for filtersize in filtersizes:
         for i in range(W - filtersize):
             for j in range(H - filtersize):
-                filter0 = output_img[i:i + filtersize,j:j + filtersize]
+                filter0 = output_img[i:i + filtersize, j:j + filtersize]
 
                 flag = 0
-                if sum(filter0[:,0]) == 0:
-                    flag +=1
-                if sum(filter0[:,filtersize - 1]) == 0:
-                    flag +=1
-                if sum(filter0[0,:]) == 0:
-                    flag +=1
-                if sum(filter0[filtersize - 1,:]) == 0:
-                    flag +=1
+                if sum(filter0[:, 0]) == 0:
+                    flag += 1
+                if sum(filter0[:, filtersize - 1]) == 0:
+                    flag += 1
+                if sum(filter0[0, :]) == 0:
+                    flag += 1
+                if sum(filter0[filtersize - 1, :]) == 0:
+                    flag += 1
                 if flag > 3:
                     output_img[i:i + filtersize, j:j + filtersize] = np.zeros((filtersize, filtersize))
 
     return output_img
+
+
+def perform_enhancement_and_morphology(img):
+    img = img_enhancement(img)
+    cv2.imshow("enhanced image", img.astype('int') * 255.0)
+    # Thinning
+    skeleton = img_skeleton(img)
+    cv2.imshow("skeleton image output", skeleton.astype('int') * 255.0)
+
+    skeleton = np.array(skeleton, dtype=np.uint8)
+    skeleton = remove_noise(skeleton)
+    cv2.imshow("skeleton after removing noise", skeleton.astype('int') * 255.0)
+    return skeleton
--- main.py
+++ main.py
@@ -1,64 +1,58 @@
 """This is the start file for project. Code execution flow start from here."""
 import cv2
+
+import descriptors
 import image_enhancement
 import input_image
-import minutiae_util
-import descriptors
-import matplotlib.pyplot as plt
 
 
+# This is the main entry point for Online finger print verification.
+# Briefly, algorithm involves following steps
+# 1. Reading 2 finger prints
+# 2. Enhancement, restoration and morphological processing
+# 3. Get minutiae from the binary, skeletonized image obtained in previous step.
+# 4. Get descriptors for minutiae points
+# 5. Matching using Brute force matcher
 def main():
-    # input_im = input_image.input_im()
-    # cv2.imshow("original image", input_im)
-    #
-    # enhanced_image = image_enhancement.img_enhancement(input_im)
-    # skeleton_image = image_enhancement.img_skeleton(enhanced_image)
-    #
-    # cv2.imshow("enhanced_image", enhanced_image.astype("int") * 255.0)
-    # cv2.imshow("enhanced skeleton image", skeleton_image.astype('int') * 255.0)
-    #
-    # # remove secluded points
-    # imgae_wo_noise = image_enhancement.remove_noise(skeleton_image)
-    # cv2.imshow("noise free image", imgae_wo_noise)
-    #
-    # ridge_endings, ridge_bifurcations, ridge_2s = minutiae_util.find_minutiae(imgae_wo_noise)
-    # minutiae_util.plot_minutiae(imgae_wo_noise, ridge_endings, ridge_bifurcations)
-    # cv2.waitKey(0)
-    img_1 = cv2.cvtColor(cv2.imread('C:/Users/Priyanka/Desktop/DIP_project/Online-finger-verfication/88.png'), cv2.COLOR_RGB2GRAY)
-    img_2 = cv2.cvtColor(cv2.imread('C:/Users/Priyanka/Desktop/DIP_project/Online-finger-verfication/1_3.png'), cv2.COLOR_RGB2GRAY)
+    # Read two images. PLEASE provide complete path containing test_input folder
+    img_1 = input_image.input_im('test_input/1/01_1.bmp')
+    img_2 = input_image.input_im('test_input/1/01_10.bmp')
 
-    kp1, des1 = descriptors.get_descriptors(img_1)
-    kp2, des2 = descriptors.get_descriptors(img_2)
+    # Enhancing the image, followed by morphological operations.
+    img_skeleton_1 = image_enhancement.perform_enhancement_and_morphology(img_1)
+    img_skeleton_2 = image_enhancement.perform_enhancement_and_morphology(img_2)
+
+    # This step finds ridge bifurcations and endings. Descriptors are obtained for these minutiae points
+    kp1, des1 = descriptors.get_descriptors(img_skeleton_1)
+    kp2, des2 = descriptors.get_descriptors(img_skeleton_2)
+
+    # Displaying key points(ridge bifurcations and endings) on image
+    img4 = cv2.drawKeypoints(img_skeleton_1.astype('uint8') * 255, kp1, outImage=None)
+    img5 = cv2.drawKeypoints(img_skeleton_2.astype('uint8') * 255, kp2, outImage=None)
+    cv2.imshow("key points for img_1", img4)
+    cv2.imshow("key points for img_2", img5)
 
     # Matching between descriptors
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
     matches = sorted(bf.match(des1, des2), key=lambda match: match.distance)
     # Plot keypoints
-    img4 = cv2.drawKeypoints(img_1, kp1, outImage=None)
-    img5 = cv2.drawKeypoints(img_2, kp2, outImage=None)
-    f, axarr = plt.subplots(1, 2)
-    axarr[0].imshow(img4)
-    axarr[1].imshow(img5)
-    plt.show()
-    # Plot matches
-    img3 = cv2.drawMatches(img_1, kp1, img_2, kp2, matches, flags=2, outImg=None)
-    plt.imshow(img3)
-    plt.show()
 
-    # Calculate score
+    # Plot matches
+    img3 = cv2.drawMatches(img_skeleton_1.astype('uint8') * 255, kp1, img_skeleton_2.astype('uint8') * 255, kp2,
+                           matches, flags=2, outImg=None)
+    cv2.imshow("matched image ", img3)
+    cv2.waitKey(0)
+
+    # Calculate score and display matching result.
     score = 0
     for match in matches:
         score += match.distance
     score_threshold = 33
+    print(score_threshold)
     if score / len(matches) < score_threshold:
         print("Fingerprint matches.")
     else:
         print("Fingerprint does not match.")
-
-
-
-
-
 
 
 if __name__ == '__main__':
